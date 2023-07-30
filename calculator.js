51a3e3608fe9b869f3dedb90186065c0013050e2
Dieal <59124097+Dieal@users.noreply.github.com>
2023-07-30 22:35:19 +0200

    Add some functionality to the calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
-let num1, num2;
+let num1 = 0;
+let num2 = 0;
 let operator;
 let displayNumber = "";
+let calculation = false;
+
+console.log(operate("+", 10, 20));
 
 const digits = document.querySelectorAll(".digit");
 digits.forEach((button) => button.addEventListener("click", e => {
@@ -8,9 +12,63 @@ digits.forEach((button) => button.addEventListener("click", e => {
     updateDisplayNumber(value);
 }));
 
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", (e) => clearData());
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((obj) => obj.addEventListener("click", (e) => {
+    
+    let op = e.target.textContent;
+    if (displayNumber === "") return;
+    
+    if (!calculation) {
+        num1 = +displayNumber;
+        resetDisplay();
+        operator = op;
+        calculation = true;
+        return;
+    } else {
+
+    }
+
+    console.log("test");
+
+
+
+}));
+
+function equalsOperation() {
+
+}
+
+function clearData() {
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+    displayNumber = "";
+    calculation = false;
+    resetDisplay();
+}
+
+function clearDisplay() {
+    const display = document.querySelector("div.display");
+    display.textContent = "";
+    displayNumber = 0;
+}
+
+function resetDisplay() {
+    const display = document.querySelector("div.display");
+    displayNumber = "0";
+    display.textContent = displayNumber
+}
+
 function updateDisplayNumber(num) {
     const display = document.querySelector("div.display");
-    displayNumber += "" + num;
+    if (+displayNumber === 0) {
+        displayNumber = num; 
+    } else {
+        displayNumber += "" + num;
+    }
     display.textContent = displayNumber;
 }
 
@@ -19,7 +77,7 @@ function operate (operator, num1, num2) {
     let result = 0;
     switch (operator) {
         case '+':
-           result = sum(num1, num2);
+           result = add(num1, num2);
            break; 
         case '-':
             result = subtract(num1, num2);
